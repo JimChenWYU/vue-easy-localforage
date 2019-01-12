c300858518c4b5b026f022120e860bac724cf172
JimChen <18219111672@163.com>
2019-01-12 18:25:29 +0800

    add release.sh

---
+++ build/release.sh
@@ -0,0 +1,28 @@
+set -e
+
+echo "Begin release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # run tests
+  # no test
+
+  # build
+  VERSION=$VERSION npm run build
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push
+  npm publish
+fi
